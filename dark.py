63a58c4618d86c0f030f275c61b469c07317d5c4
marwa-404 <marwacyber74@gmail.com>
2024-10-12 16:52:33 +0500

    Add files via upload

---
+++ dark.py
@@ -0,0 +1,36 @@
+import os
+import platform
+import subprocess
+import time
+
+subprocess.call('clear', shell=True)
+
+print('Checking for updates...')
+
+subprocess.call('git pull', shell=True)
+
+
+permission = input('Do you want to allow permissions? (y/n): ')
+if permission.lower() == 'y':
+    subprocess.call('termux-setup-storage', shell=True)
+
+try:
+    import requests
+except ImportError:
+    subprocess.call('pip install requests', shell=True)
+
+subprocess.call('git pull', shell=True)
+
+
+bit = platform.architecture()[0]
+if bit == '64bit':
+    print("\nCongratulations! Your device supports this tool.")
+    from DARK import menu___
+    menu___()
+elif bit == '32bit':
+    print("\nCongratulations! Your device supports this tool.")
+    from DARK32 import menu___
+    menu___()
+else:
+    print("Just Leave:", bit)
+
